e0d3b173ae158c8d79adfbf50657f1afb7ec05bf
seieric <seiya@yaseiblog.org>
2019-11-04 18:36:44 +0900

    Add license

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # php-easy-login
 HTMLやPHPファイルの先頭に数行追記するだけでログイン認証が行えるベーシックなログインシステム
+
+# license
+MIT
---
+++ login.php
@@ -0,0 +1,8 @@
+<?php
+/**
+*PHP easy login
+*Copyright (c) 2019 seieric
+*This software is released under the MIT License.
+*http://opensource.org/licenses/mit-license.php
+*/
+ ?>
