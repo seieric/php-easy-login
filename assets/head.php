b206301e09e7b0e6bfa671671be516c8af63e2f3
seieric <30688855+seieric@users.noreply.github.com>
2019-11-07 20:19:11 +0900

    Collect an error

--- assets/head.php
+++ assets/head.php
@@ -2,7 +2,7 @@
 <html lang="ja" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title><?php get_title(); ?></title>
+    <title><?php echo_title(); ?></title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="assets/common.js"></script>
   </head>
